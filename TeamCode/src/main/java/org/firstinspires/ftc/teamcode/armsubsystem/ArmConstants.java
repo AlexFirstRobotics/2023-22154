2845d1bfd9cf0634650572beba742ab92eb0f748
AlexFirstRobotics <crossalex@waylandunion.org>
2023-11-16 18:32:30 -0500

    Positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armsubsystem/ArmConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armsubsystem/ArmConstants.java
@@ -5,10 +5,10 @@ public final class ArmConstants {
     public static class ARM_POSITIONS {
         public static class PIVOT {
             public static int HOME_POSITION = 30;
-            public static int SCORE_POSITION = 777;
+            public static int SCORE_POSITION = 830;
             public static int SCORE_EXTRA_POSITION = -900;
             public static int PICKUP_POSITION = 194; //-100
-            public static int CLIMB_UP_POSITION = -1100;
+            public static int CLIMB_UP_POSITION = 1150;
             public static int CLIMB_DOWN_POSITION = 100;
         }
         public static class EXTEND {
